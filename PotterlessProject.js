4ff8bff68086af782b9fe5fa664607536468ee9b
KConlon13 <KyleConlon13@gmail.com>
2020-01-14 13:52:20 -0500

    added Potterless bio and edited the new image for formatting

--- PotterlessProject.js
+++ PotterlessProject.js
@@ -9,21 +9,19 @@ class PotterlessProject extends Component {
         return (
             <View>
                 <Text style={styles.videoTitle} onPress={()=>Linking.openURL("https://www.youtube.com/watch?v=RsICl8cgj2s")}>Potterless</Text>
-                <Image source={require("./PotterlessScreenshot.png")} containerStyle={{ width: 370, height: 200, display: "block", marginLeft:"auto", marginRight:"auto" }} PlaceholderContent={ <ActivityIndicator /> } placeholderStyle={{ height: "100%", width: "100%" }} />
+                <Image source={require("./PotterlessScreenshot.png")} containerStyle={{ width: 370, height: 185, display: "block", marginLeft:"auto", marginRight:"auto" }} PlaceholderContent={ <ActivityIndicator /> } placeholderStyle={{ height: "100%", width: "100%" }} />
                 <View style={styles.githubDemoLine}>
                     <Text style={styles.githubDemoLink} onPress={()=>Linking.openURL("https://github.com/KConlon13/Mod2PotterlessProject")}>Github</Text>
                     <Text style={styles.githubDemoDividerLine}> | </Text>
                     <Text style={styles.githubDemoLink} onPress={()=>Linking.openURL("https://www.youtube.com/watch?v=RsICl8cgj2s")} >Demo</Text>
                 </View>
-                <Text style={styles.projectBio}>This is a little write up describing what this project is about. Its going to have words and adjectives and nouns and lots descriptive words oh boy. Coming soon!</Text>
+                <Text style={styles.projectBio}>The Wizarding World (aka Warner Bros) have done an incredible job creating interactive platforms to keep Harry Potter fans satisfied for years now- whether it be the immersive amusement park in Universal Studios, the epic book/movie series, or their online presence... namely the Pottermore website. This inspired the building of Potterless (the name implying it provides some Harry Potter fun but not any more than Pottermore)- a light-hearted attempt to create an engaging and themed experience that could (almost) rival or imitate what the Pottermore website does so successfully.</Text>
                 <Card title="What I Used" titleStyle={{ color: "black"}} containerStyle={{ borderColor: "#31CCC8", marginTop: 10, marginBottom: 20}} >
                     <View style={{ alignItems: "center" }}>
-                        <Text style={styles.videoList}>React.js</Text>
-                        <Text style={styles.videoList}>Javascript</Text>
                         <Text style={styles.videoList}>Ruby</Text>
                         <Text style={styles.videoList}>Rails</Text>
                         <Text style={styles.videoList}>HTML & CSS</Text>
-                        <Text style={styles.videoList}>Semantic UI Framework</Text>
+                        <Text style={styles.videoList}>Bootstrap UI Framework</Text>
                     </View>
                 </Card>
                 <Text style={styles.dividerLine}>________________________</Text>
